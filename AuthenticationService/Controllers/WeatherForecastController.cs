1a5933f97e6e587b5fab30136d29deb35c520ee3
Daniil <rychenkov16@yandex.ru>
2025-04-09 22:43:31 +0300

    add User

---
+++ AuthenticationService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,25 @@
+﻿using AuthenticationService.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationService.Controllers
+{
+    public class WeatherForecastController : ControllerBase
+    {
+        
+        public WeatherForecastController()
+        {
+            var logger = new Logger();
+
+            logger.WriteEvent("Сообщение о событии в программе");
+            logger.WriteError("Сообщение об ошибки в программе");
+        }
+
+        
+    }
+}
